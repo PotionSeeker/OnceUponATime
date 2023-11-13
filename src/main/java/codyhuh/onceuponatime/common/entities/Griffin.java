2a69caec2368105c621759fde0b98c3fa4b4011a
Coda <cody.koelliker@outlook.com>
2023-11-12 23:35:22 -0700

    griffin flying is pretty much done

--- src/main/java/codyhuh/onceuponatime/common/entities/Griffin.java
+++ src/main/java/codyhuh/onceuponatime/common/entities/Griffin.java
@@ -8,6 +8,8 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
@@ -22,13 +24,14 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.event.ForgeEventFactory;
 
+// todo - figure out a way to make it descend smoothly (right now, it changes its flying state to false, and falls to the ground...)
 public class Griffin extends AbstractHorse {
     private static final EntityDataAccessor<Boolean> IS_FLYING = SynchedEntityData.defineId(Griffin.class, EntityDataSerializers.BOOLEAN);
+    private static final EntityDataAccessor<Integer> FLIGHT_TICKS = SynchedEntityData.defineId(Griffin.class, EntityDataSerializers.INT);
     public final int MAX_FLIGHT_TICKS = 1200;
     private GriffinWanderGoal wanderGoal;
-    public int flightTicks;
-    public int onGroundTicks;
 
     public Griffin(EntityType<? extends AbstractHorse> type, Level level) {
         super(type, level);
@@ -49,29 +52,32 @@ public class Griffin extends AbstractHorse {
     }
 
     public static AttributeSupplier.Builder createGriffinAttributes() {
-        return Mob.createMobAttributes().add(Attributes.MAX_HEALTH, 40.0D).add(Attributes.FLYING_SPEED, 1.0F).add(Attributes.MOVEMENT_SPEED, 0.2F);
+        return Mob.createMobAttributes().add(Attributes.MAX_HEALTH, 40.0D).add(Attributes.JUMP_STRENGTH, 1.0D).add(Attributes.FLYING_SPEED, 1.0F).add(Attributes.MOVEMENT_SPEED, 0.2F);
     }
 
     public boolean wantsToFly() {
-        return canFly() && flightTicks <= 1200;
+        return canFly() && getFlightTicks() <= MAX_FLIGHT_TICKS;
     }
 
     @Override
     protected void defineSynchedData() {
         super.defineSynchedData();
         this.entityData.define(IS_FLYING, false);
+        this.entityData.define(FLIGHT_TICKS, 0);
     }
 
     @Override
     public void addAdditionalSaveData(CompoundTag pCompound) {
         super.addAdditionalSaveData(pCompound);
         pCompound.putBoolean("IsFlying", this.isFlying());
+        pCompound.putInt("FlightTicks", this.getFlightTicks());
     }
 
     @Override
     public void readAdditionalSaveData(CompoundTag pCompound) {
         super.readAdditionalSaveData(pCompound);
         this.setFlying(pCompound.getBoolean("IsFlying"));
+        this.setFlightTicks(pCompound.getInt("FlightTicks"));
     }
 
     public boolean isFlying() {
@@ -80,6 +86,14 @@ public class Griffin extends AbstractHorse {
 
     public void setFlying(boolean flying) {
         this.entityData.set(IS_FLYING, flying);
+    }
+
+    public int getFlightTicks() {
+        return this.entityData.get(FLIGHT_TICKS);
+    }
+
+    public void setFlightTicks(int flightTicks) {
+        this.entityData.set(FLIGHT_TICKS, flightTicks);
     }
 
     public boolean canFly() {
@@ -97,14 +111,19 @@ public class Griffin extends AbstractHorse {
     }
 
     @Override
+    public void setNoGravity(boolean pNoGravity) {
+        super.setNoGravity(isFlying());
+    }
+
+    @Override
     public void tick() {
         super.tick();
 
-        if (isFlying() && !isVehicle()) {
-            flightTicks++;
+        if (getFlightTicks() <= MAX_FLIGHT_TICKS && isFlying() && !isVehicle()) {
+            setFlightTicks(getFlightTicks() + 1);
         }
 
-        if (!canFly()) {
+        if (getFlightTicks() >= MAX_FLIGHT_TICKS) {
             setFlying(false);
         }
 
@@ -112,12 +131,8 @@ public class Griffin extends AbstractHorse {
             setDeltaMovement(getDeltaMovement().subtract(0.0D, 0.005D, 0.0D));
         }
 
-        if (!isFlying() && !isVehicle()) {
-            flightTicks--;
-        }
-
-        if (flightTicks <= -MAX_FLIGHT_TICKS) {
-            flightTicks = 0;
+        if (getFlightTicks() > 0 && !isFlying() && !isVehicle()) {
+            setFlightTicks(getFlightTicks() - 1);
         }
 
         double x = getDeltaMovement().x();
@@ -128,6 +143,15 @@ public class Griffin extends AbstractHorse {
         if (wanderGoal != null && isFlying() && wantsToFly() && !isVehicle() && notMoving) {
             wanderGoal.trigger();
         }
+    }
+
+    @Override
+    public InteractionResult mobInteract(Player pPlayer, InteractionHand pHand) {
+        if (pPlayer.getItemInHand(pHand).is(Items.RABBIT) && !ForgeEventFactory.onAnimalTame(this, pPlayer)) {
+            tameWithName(pPlayer);
+        }
+
+        return super.mobInteract(pPlayer, pHand);
     }
 
     @Override
